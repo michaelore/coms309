615eace47c4d3d98b9f652d8b95fca8515d2ca47
Michael Ore <selbram1993@gmail.com>
2012-03-17 20:25:55 -0500

    Created many-to-many relation between locations and routes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
 	has_many :coordinates
+	has_and_belongs_to_many :routes
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,4 @@
 class Route < ActiveRecord::Base
 	serialize :vertices
+	has_and_belongs_to_many :locations
 end
