4e1bf16cd8f01d93575766db6e48f999adf8c9dc
Michael Ore <selbram1993@gmail.com>
2012-03-19 18:20:13 -0500

    Completed coordinate_controller.rb, not tested

--- app/controllers/coordinate_controller.rb
+++ app/controllers/coordinate_controller.rb
@@ -1,14 +1,37 @@
 class CoordinateController < ApplicationController
   def create
+    @coordinate = Coordinate.new(params[:post])
+    respond_to do |format|
+      if @coordinate.save
+        format.json { head :created, :location => @coordinate }
+      else
+        format.json { render :json => @coordinate.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @coordinate = Coordinate.find(params[:id])
+    @coordinate.destroy
+
+    respond_to do |format|
+      format.json { head :ok }
+    end
   end
 
   def index
+    @coordinates = Coordinate.find(:all)
+    respond_to do |format|
+      format.html
+      format.json { render :json => @coordinates }
+    end
   end
 
   def show
+    @coordinates = Coordinate.find(params[:id])
+    respond_to do |format|
+      format.json { render :json => @coordinate }
+    end
   end
 
 end
