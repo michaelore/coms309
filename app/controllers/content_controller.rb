f2100ab7881c4aedd3c7fec771e2d0fb3362faab
Michael Ore <selbram1993@gmail.com>
2012-03-27 09:23:46 -0500

    Removed redirections in content_controller.rb

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -4,22 +4,18 @@ class ContentController < ApplicationController
   end
 
   def home
-    redirect_to :action => 'login' unless session[:logged_in]
     render :file => 'content/mobilehome'
   end
 
   def search
-    redirect_to :action => 'login' unless session[:logged_in]
     render :file => 'content/mobilesearch'
   end
 
   def history
-    redirect_to :action => 'login' unless session[:logged_in]
     render :file => 'content/mobilehistory'
   end
 
   def map
-    redirect_to :action => 'login' unless session[:logged_in]
     render :file => 'content/mobilemap'
   end
 end
