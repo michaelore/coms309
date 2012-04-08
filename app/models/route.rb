e028e651daeeebb44281c7b1514fffe7ed999f9a
Michael Ore <selbram1993@gmail.com>
2012-04-08 13:14:58 -0500

    Added recursive composite route finder

--- app/models/route.rb
+++ app/models/route.rb
@@ -8,4 +8,16 @@ class Route < ActiveRecord::Base
 		vertices = self.vertices.map {|id| Coordinate.find(id)}
 		return {"start" => start, "end" => ending, "vertices" => vertices}
 	end
+
+	def self.getCompositeRoutes(startID, endID, depth)
+		start = Location.find(startID)
+		ending = Location.find(endID)
+		return [[]] unless startID != endID
+		return [] unless depth > 0
+		comps = []
+		start.routes_leaving.each do |r|
+			comps.concat(getCompositeRoutes(r.end_id, endID, depth-1).map {|comp| [r].concat(comp)})
+		end
+		return comps
+	end
 end
