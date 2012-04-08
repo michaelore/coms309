403d234b1c37b75423907dcea005e21202b86ad6
Michael Ore <selbram1993@gmail.com>
2012-04-08 14:20:02 -0500

    Changed name of end to ending

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
 	has_many :coordinates
 	has_many :routes_leaving, :class_name => "Route", :inverse_of => :start, :foreign_key => :start_id
-	has_many :routes_entering, :class_name => "Route", :inverse_of => :end, :foreign_key => :end_id
+	has_many :routes_entering, :class_name => "Route", :inverse_of => :ending, :foreign_key => :ending_id
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,13 +1,13 @@
 class Route < ActiveRecord::Base
 	serialize :vertices
 	belongs_to :start, :class_name => "Location", :inverse_of => :routes_leaving
-	belongs_to :end, :class_name => "Location", :inverse_of => :routes_entering
+	belongs_to :ending, :class_name => "Location", :inverse_of => :routes_entering
 
 	def distance
 		lat1 = start.latitude*Math.pi/180
 		lon1 = start.longitude*Math.pi/180
-		lat2 = end.latitude*Math.pi/180
-		lon2 = end.longitude*Math.pi/180
+		lat2 = ending.latitude*Math.pi/180
+		lon2 = ending.longitude*Math.pi/180
 		a = Math.sin((lat2-lat1)/2)**2+Math.cos(lat1)*Math.cos(lat2)*sin((lon2-lon1)/2)**2
 		c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
 		return 6367500*c
@@ -15,19 +15,19 @@ class Route < ActiveRecord::Base
 
 	def deep
 		start = Location.find(self.start_id)
-		ending = Location.find(self.end_id)
+		ending = Location.find(self.ending_id)
 		vertices = self.vertices.map {|id| Coordinate.find(id)}
-		return {"start" => start, "end" => ending, "vertices" => vertices}
+		return {"start" => start, "ending" => ending, "vertices" => vertices}
 	end
 
-	def self.getCompositeRoutes(startID, endID, depth)
+	def self.getCompositeRoutes(startID, endingID, depth)
 		start = Location.find(startID)
-		ending = Location.find(endID)
-		return [[]] unless startID != endID
+		ending = Location.find(endingID)
+		return [[]] unless startID != endingID
 		return [] unless depth > 0
 		comps = []
 		start.routes_leaving.each do |r|
-			comps.concat(getCompositeRoutes(r.end_id, endID, depth-1).map {|comp| [r].concat(comp)})
+			comps.concat(getCompositeRoutes(r.ending_id, endingID, depth-1).map {|comp| [r].concat(comp)})
 		end
 		return comps
 	end
