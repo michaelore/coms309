392aea4e1c567b44f74340da860696b003306078
Michael Ore <selbram1993@gmail.com>
2012-04-08 08:32:13 -0500

    Fixed relations for location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
 	has_many :coordinates
-	has_many :routes_leaving, :class_name => "Route", :inverse_of => :start
-	has_many :routes_entering, :class_name => "Route", :inverse_of => :end
+	has_many :routes_leaving, :class_name => "Route", :inverse_of => :start, :foreign_key => :start_id
+	has_many :routes_entering, :class_name => "Route", :inverse_of => :end, :foreign_key => :end_id
 end
