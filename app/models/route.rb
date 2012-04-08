9c51cf26fe0a1e9047753e68e15c14783234bcbc
Michael Ore <selbram1993@gmail.com>
2012-04-08 14:13:34 -0500

    Added distance calculation method

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,6 +2,17 @@ class Route < ActiveRecord::Base
 	serialize :vertices
 	belongs_to :start, :class_name => "Location", :inverse_of => :routes_leaving
 	belongs_to :end, :class_name => "Location", :inverse_of => :routes_entering
+
+	def distance
+		lat1 = start.latitude*Math.pi/180
+		lon1 = start.longitude*Math.pi/180
+		lat2 = end.latitude*Math.pi/180
+		lon2 = end.longitude*Math.pi/180
+		a = Math.sin((lat2-lat1)/2)**2+Math.cos(lat1)*Math.cos(lat2)*sin((lon2-lon1)/2)**2
+		c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+		return 6367500*c
+	end
+
 	def deep
 		start = Location.find(self.start_id)
 		ending = Location.find(self.end_id)
