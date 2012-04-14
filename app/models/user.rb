c4669e426ced70bbee70a6b064615d93f2174852
Michael Ore <selbram1993@gmail.com>
2012-04-13 20:56:23 -0500

    Added many-to-many relations for users and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :ratings
+  has_many :ratees, :class_name => "Route", :through => :ratings, :source => :route
   has_many :usages
+  has_many :affiliatees, :class_name => "Route", :through => :usages, :source => :route
 end
