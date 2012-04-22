5de0a3d118e90e6ab8b23c4db2ab520566ee3d8a
Bo Wu <wubo@iastate.edu>
2012-04-22 15:34:33 -0500

    Everything correct

--- lib/RouteRanking.rb
+++ lib/RouteRanking.rb
@@ -1,33 +1,39 @@
 class RouteRanking
 
+	##
+	#function initialize
+	
 	def initialize
 		@routes = []
 		@sortedRoutes = []
 		@averageRank = []
-		@count = 0
 	end
 
+	##
+	#add route in the routes
+	
 	def addRoute(id, likes, dislikes)
 		@routes.push({:id => id, :likes => likes, :dislikes => dislikes})
-		average = likes * 1.0 / (likes + dislikes)
-		count++
+		average = likes * 1.0 / (likes + dislikes + 1)
 		@averageRank.push({:id => id, :average => average})
 	end
 	
+	##
+	#sort route by average ranking and return the sourted routes
+	
 	def sort
 		
-		for j in 1..count
-			key = averageRank.i.average
+		for j in 1..(@averageRank.count-1)
+			key = @averageRank[j][:average]
 			i = j - 1
-			while i > 0 && averageRank.i.average > key do
-				averageRank.i+1.average = averageRank.i.average
+			while i >= 0 && @averageRank[i][:average] < key do
+				@averageRank[i+1][:average] = @averageRank[i][:average]
 				i = i - 1
 			end
-			averageRank.i+1.average = key
+			@averageRank[i+1][:average] = key
 		end
 		
-		
-		return @sortRoutes
+		return @averageRank
 	end
 	
 end
