0136110f6916bafa4bb7104bae31fc497c4aeda5
Bo Wu <wubo@iastate.edu>
2012-04-22 16:01:13 -0500

    Fix the id problem

--- lib/RouteRanking.rb
+++ lib/RouteRanking.rb
@@ -14,7 +14,7 @@ class RouteRanking
 	
 	def addRoute(id, likes, dislikes)
 		@routes.push({:id => id, :likes => likes, :dislikes => dislikes})
-		average = likes * 1.0 / (likes + dislikes + 1)
+		average = likes * 1.0 / (likes + dislikes)
 		@averageRank.push({:id => id, :average => average})
 	end
 	
@@ -25,12 +25,13 @@ class RouteRanking
 		
 		for j in 1..(@averageRank.count-1)
 			key = @averageRank[j][:average]
+			key_id = @averageRank[j][:id]
 			i = j - 1
 			while i >= 0 && @averageRank[i][:average] < key do
-				@averageRank[i+1][:average] = @averageRank[i][:average]
+				@averageRank[i+1] = @averageRank[i]
 				i = i - 1
 			end
-			@averageRank[i+1][:average] = key
+			@averageRank[i+1] = {:average => key, :id => key_id}
 		end
 		
 		return @averageRank
