3197355bf10e1e9fa563b9aad89a0b333ac34d84
Michael Ore <selbram1993@gmail.com>
2012-04-22 18:22:26 -0500

    Routes now sorted.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 require 'Agent.rb'
+require 'RouteRanking.rb'
 
 module ApplicationHelper
 	def isMobile
 		return Agent.new(request.env['HTTP_USER_AGENT']).os_s
+	end
+
+	def sortRoutes(routes)
+		rr = RouteRanking.new
+		for r in routes
+			rr.addRoute(r.id, r.likes, r.dislikes)
+		end
+		return rr.sort.map {|d| d[:id]}
 	end
 end
