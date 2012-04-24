90dd87f3c66fccc835f112a4fbbf14c4b00b6997
Christopher Van Oort <cvocvo@gmail.com>
2012-04-23 20:36:01 -0500

    fixed typo
    Signed-off-by: Christopher Van Oort <cvocvo@gmail.com>

--- public/javascripts/mobile/ajax.js
+++ public/javascripts/mobile/ajax.js
@@ -92,8 +92,8 @@ function pullLocation(id){
 */
 
 function pullRouteData(id){
-http://digitravel.herokuapp.com/routes/3.json
-{"start":"UDCC","ending":"Parks Library","likes":0,"dislikes":0,"favorite":0,"distance":434.3532741670897}
+//http://digitravel.herokuapp.com/routes/3.json
+//{"start":"UDCC","ending":"Parks Library","likes":0,"dislikes":0,"favorite":0,"distance":434.3532741670897}
 	var $id = id;
 	$.get("/routes/" + $id + ".json",
    function(data){
