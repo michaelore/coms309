3121aec84dd8ca191dfcdb97a7fde1ecb81301fe
Joe Ross <jsross11@iastate.edu>
2012-04-24 21:48:54 -0500

    some more fixes to bugs
    Signed-off-by: Joe Ross <jsross11@iastate.edu>

--- public/javascripts/mobile/ajax.js
+++ public/javascripts/mobile/ajax.js
@@ -34,8 +34,8 @@ function postLocation(lat, lng, name){
         dataType: 'json',
         success: function(msg) {
 			alert( "Data Saved: " + msg );
-			window.parent.$routeID = msg.id;
-			alert("Postlocation::  "+msg.id)
+			window.parent.$routeID = msg.location.id;
+			alert("Postlocation::  "+ msg.location.id)
         },
 		error: function(msg) {
 			alert( "Error: " + msg );
