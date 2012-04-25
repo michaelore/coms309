e7600e68349760c500d7bd53d2076bc69da18b05
Christopher Van Oort <cvocvo@gmail.com>
2012-04-24 22:00:08 -0500

    made changes to icon setter for like/dislike/favorites
    Signed-off-by: Christopher Van Oort <cvocvo@gmail.com>

--- public/javascripts/mobile/ajax.js
+++ public/javascripts/mobile/ajax.js
@@ -79,25 +79,32 @@ function ldfav(id, todo){
 		if (data.liked == 1)
 		{
 			//switch like button to checkmark and dislike button to arrow-d
+			$("#like").data('icon', 'check'); 
 			$("#like .ui-icon").addClass("ui-icon-check").removeClass("ui-icon-arrow-u");
+			$("#dislike").data('icon', 'arrow-d'); 
 			$("#dislike .ui-icon").addClass("ui-icon-arrow-d").removeClass("ui-icon-check");
 		}
 		else if (data.liked == 0)
 		{
 			//set like button to arrow-u and dislike button to arrow-d
+			$("#like").data('icon', 'arrow-u'); 
 			$("#like .ui-icon").addClass("ui-icon-arrow-u").removeClass("ui-icon-check");
+			$("#dislike").data('icon', 'arrow-d'); 
 			$("#dislike .ui-icon").addClass("ui-icon-arrow-d").removeClass("ui-icon-check");
 		}
 		else
 		{
 			//dislike is currently selected b/c data.liked == -1
 			//set dislike button to check and like to arrow-u
+			$("#like").data('icon', 'arrow-u'); 
 			$("#like .ui-icon").addClass("ui-icon-arrow-u").removeClass("ui-icon-check");
+			$("#dislike").data('icon', 'check'); 
 			$("#dislike .ui-icon").addClass("ui-icon-check").removeClass("ui-icon-arrow-u");
 		}
 		if (data.favorite == 1)
 		{
 			//set favorite icon to check b/c it's currently favorited
+			$("#favorite").data('icon', 'check'); 
 			$("#favorite .ui-icon").addClass("ui-icon-check").removeClass("ui-icon-star");
 		}
 		else
@@ -125,33 +132,39 @@ function pullRouteData(id){
 		if (data.liked == 1)
 		{
 			//switch like button to checkmark and dislike button to arrow-d
+			$("#like").data('icon', 'check'); 
 			$("#like .ui-icon").addClass("ui-icon-check").removeClass("ui-icon-arrow-u");
+			$("#dislike").data('icon', 'arrow-d'); 
 			$("#dislike .ui-icon").addClass("ui-icon-arrow-d").removeClass("ui-icon-check");
 		}
 		else if (data.liked == 0)
 		{
 			//set like button to arrow-u and dislike button to arrow-d
+			$("#like").data('icon', 'arrow-u'); 
 			$("#like .ui-icon").addClass("ui-icon-arrow-u").removeClass("ui-icon-check");
+			$("#dislike").data('icon', 'arrow-d'); 
 			$("#dislike .ui-icon").addClass("ui-icon-arrow-d").removeClass("ui-icon-check");
 		}
 		else
 		{
 			//dislike is currently selected b/c data.liked == -1
 			//set dislike button to check and like to arrow-u
+			$("#like").data('icon', 'arrow-u'); 
 			$("#like .ui-icon").addClass("ui-icon-arrow-u").removeClass("ui-icon-check");
+			$("#dislike").data('icon', 'check'); 
 			$("#dislike .ui-icon").addClass("ui-icon-check").removeClass("ui-icon-arrow-u");
 		}
 		if (data.favorite == 1)
 		{
 			//set favorite icon to check b/c it's currently favorited
+			$("#favorite").data('icon', 'check'); 
 			$("#favorite .ui-icon").addClass("ui-icon-check").removeClass("ui-icon-star");
 		}
 		else
 		{
 			//otherwise it's not favorited, so we need to set the icon to the star
 			$("#favorite .ui-icon").addClass("ui-icon-star").removeClass("ui-icon-check");
-		}
-		
+		}		
 		//set the distance portion of the page
 		document.getElementById('distance').innerHTML = "Distance: " + data.distance + " meters";
    });
